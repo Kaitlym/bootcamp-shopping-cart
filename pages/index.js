63d4d6516eaa6560e5f87d8993299b49ffc25d44
Kaitlyn Martinez (Intern) <kmartinez1@godaddy.com>
2021-07-13 08:05:25 -0700

    add to cart and remove from cart working

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,27 @@
 import React from 'react';
 import Head from '../components/head';
 
+import Link from 'next/link';
+
+import { Container, Typography } from '@material-ui/core'
+
+
 export const IndexPage = () => (
   <div>
     <Head title='Home'/>
-    <h1>Welcome!</h1>
+    <div>
+      <Typography variant="h3">Home</Typography>   
+    </div>
+    <div>
+      <Link href="/cart">
+        <a>View cart</a>
+      </Link>
+    </div>
+    <div>
+      <Link href="/shop">
+        <a>View shop</a>
+      </Link>
+    </div>
   </div>
 );
 
