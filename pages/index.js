c7f2262488c34b83f20f690cd6de827f11a6a673
Kaitlyn Martinez (Intern) <kmartinez1@godaddy.com>
2021-07-15 06:41:03 -0700

    total and edit are up

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,18 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
-import Head from '../components/head';
-
-import Link from 'next/link';
 
 import { Container, Typography } from '@material-ui/core'
 
+import NavBar from '../components/NavBar';
+
+
 
 export const IndexPage = () => (
-  <div>
-    <Head title='Home'/>
-    <div>
-      <Typography variant="h3">Home</Typography>   
-    </div>
-    <div>
-      <Link href="/cart">
-        <a>View cart</a>
-      </Link>
-    </div>
-    <div>
-      <Link href="/shop">
-        <a>View shop</a>
-      </Link>
-    </div>
-  </div>
+  <Container>
+    <NavBar
+      name={"Home"}
+    />
+  </Container>
 );
 
 export default IndexPage;
